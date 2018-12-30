0c1e6a311b07d0511c9f85c7fa8fc36167c2d9f7
hoppertje <michaelhop1997@gmail.com>
2018-12-30 01:12:09 +0100

    Start met score naar server sturen

--- public/javascripts/test_button.js
+++ public/javascripts/test_button.js
@@ -2,20 +2,45 @@
 $(document).ready(function() {
 
 });
+var CorrectWords = [];
+let score= 0;
+
+var timeleft = 10;
+var downloadTimer = setInterval(function(){
+    document.getElementById("progressBar").value = 10 - --timeleft;
+    if(timeleft <= 0){
+        clearInterval(downloadTimer);
+        document.getElementById('info').innerHTML = "Je tijd is om"};
+},1000);
+
 
 function ButtonPressed(element) {
     console.log("button pressed");
     console.log(element.innerHTML)
     if (element.checked == 'false') {
-        element.style.backgroundColor = "black";
+        element.style.backgroundColor = "green";
         element.checked = 'true';
+        score ++
+        document.getElementById('score').innerHTML = score;
     }
-    else {
-        element.style.backgroundColor = "red";
+    else if(element.checked == 'true') {
+        element.style.backgroundColor = "grey";
         element.checked = 'false';
+        score --
+        document.getElementById('score').innerHTML = score;
     }
+    else{
+        element.style.backgroundColor = "green";
+        element.checked = 'true';
+        score ++
+        document.getElementById('score').innerHTML = score;
+    }
+
     ;
 }
 
+//eerste kijkt of checked false is en zet het op true
+//tweede kijkt andersom
+//derde is nodig omdat in het begin checked null is
 
 
--- routes/test_button.js
+++ routes/test_button.js
@@ -4,20 +4,33 @@ var database_operations = require('./../database_operations');
 
 
 var test_word = "hop";
-var words =["hond" ,"kat","vogel"];
+var words =[];
+var team_name = ["michael","shannon"]
+var team_score= [0,0];
 
-var all_words = 'Hop (Humulus lupulus) is een plant uit de hennepfamilie (Cannabaceae), die in Nederland en België in het wild voorkomt, en hier vroeger ook veel geteeld werd. De hopbellen (vruchtkegels) worden als conserveer- en smaakmiddel gebruikt bij de bereiding van bier. Hop is een vaste plant die overwintert als wortelstok. De soort komt van nature voor in het grootste deel van de gematigde- en koude zone van het noordelijk halfrond, ten noorden van de 32e breedtegraad.[1][2][3] Door de teelt ten behoeve van de bierproductie heeft de hop zijn areaal uitgebreid naar Zuid-Amerika, Zuid-Afrika, Australië en Tasmanië.',
+
+var all_words = 'Hop Humulus lupulus is een plant uit de hennepfamilie Cannabaceae, die in Nederland en België in het wild voorkomt, en hier vroeger ook veel geteeld werd. De hopbellen (vruchtkegels) worden als conserveer- en smaakmiddel gebruikt bij de bereiding van bier. Hop is een vaste plant die overwintert als wortelstok. De soort komt van nature voor in het grootste deel van de gematigde- en koude zone van het noordelijk halfrond, ten noorden van de 32e breedtegraad.[1][2][3] Door de teelt ten behoeve van de bierproductie heeft de hop zijn areaal uitgebreid naar Zuid-Amerika, Zuid-Afrika, Australië en Tasmanië.',
     wordArray = all_words.split(' ');
     console.log(wordArray);
 
+//function SelectWords(){
+//    var selection = wordArray.slice(1, 3);
+//    return selection;
+//}
 
+words = wordArray.slice(0,5);
 
 // Home page route.
 router.get('/', function (req, res, next) {
     res.render('test_button',{test_word, words});
 });
 
-
+//Hieronder is nog niet compleet
+router.post('/submit',function (req, res) {
+    team_score[0]++;
+    console.log(team_score);
+    res.render('test_button',{test_word, words});
+});
 
 
 module.exports = router;
