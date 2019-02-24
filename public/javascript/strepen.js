ce4f99119304960711d514d8923b7701706466c5
thomas6886 <thomas.6886@hotmail.com>
2019-02-24 17:40:50 +0100

    Strepen!

--- public/javascript/strepen.js
+++ public/javascript/strepen.js
@@ -4,18 +4,29 @@ var currLading = null;
 function selectLading(ladingID){
     if(currLading != ladingID){
         currLading = ladingID;
+        document.getElementById('lading_'+ladingID).classList.add('selected');
+        console.log(document.getElementById('lading_'+ladingID));
+
     }else{
+        document.getElementById('lading_'+ladingID).classList.remove('selected');
+        console.log(document.getElementById('lading_'+ladingID));
         currLading = null;
     }
     console.log(currLading);
+}
+
+function selectByElementID(elementID){
+    document.getElementById(elementID).classList.add(" selected");
 }
 
 function selectUser(userID){
     if(currLading != null){
         if(!currUsers.includes(userID)){
             currUsers.push(userID);
+            document.getElementById('user_'+userID).classList.add('selected');
         }else{
             currUsers.splice(currUsers.indexOf(userID), 1);
+            document.getElementById('user_'+userID).classList.remove('selected');
         }
 
     }else{
@@ -34,7 +45,7 @@ function populateUsers(){
     $.getJSON( '/api/users', function( data ) {
         $.each(data, function(){
             userContent += '<div class="col-2 itemwrapper">';
-            userContent += '<div class="itemblock user" id='+this.USERID+' onclick="selectUser('+this.USERID+')">';
+            userContent += '<div class="itemblock user" id=user_'+this.USERID+' onclick="selectUser('+this.USERID+')">';
             userContent += '<div class="topRowInfo"><p>12</p><p>18</p></div>';
             userContent += '<img class="profilePicture" src="pictures/profile_pictures/' + this.PLAATJE + '">';
             userContent += '<div class="bottomRowInfo"><p>'+this.NAAM+'</p></div>';
@@ -49,7 +60,7 @@ function populateLadingen(){
     $.getJSON( '/api/ladingen/active', function( data ) {
         $.each(data, function(){
             ladingenContent += '<div class="col-2 itemwrapper">';
-            ladingenContent += '<div class="itemblock button lading" id='+this.LADINGID+' onclick="selectLading('+this.LADINGID+')">';
+            ladingenContent += '<div class="itemblock button lading" id=lading_'+this.LADINGID+' onclick="selectLading('+this.LADINGID+')">';
             ladingenContent += '<img class="profilePicture" src="pictures/merken/' + this.PLAATJE + '">';
             ladingenContent += '</div></div>'
         });
@@ -58,9 +69,27 @@ function populateLadingen(){
 }
 
 function streep(){
-    for(let i=0; i<currUsers.length; i++){
-        createStreep(currUsers[i], 1, currLading);
+    if((currLading != null)&&currUsers.length > 0){
+        for(let i=0; i<currUsers.length; i++){
+            createStreep(currUsers[i], 1, currLading);
+        }
+        clearSelection();
+        swal({
+            title: "Gestreept",
+            text: "Geniet ervan!",
+            type: "success",
+            timer: 2000
+        })
     }
+}
+
+function clearSelection(){
+    for(let i=0; i<currUsers.length; i++){
+        document.getElementById('user_'+currUsers[i]).classList.remove('selected');
+    }
+    document.getElementById('lading_'+currLading).classList.remove('selected');
+    currUsers = [];
+    currLading = null;
 }
 
 function createStreep(userid, aantal, ladingid){
