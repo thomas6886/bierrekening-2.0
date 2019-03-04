7f9657e30c1a4ee5857c28270b1982a63b772f60
hoppertje <michaelhop1997@gmail.com>
2019-03-04 23:27:47 +0100

    UserID vervangen met Naam

--- routes/test.js
+++ routes/test.js
@@ -1,36 +1,19 @@
 
-var a = [
-    {id: 1, name: 'Greg'},
-    {id: 2, name: 'David'},
-    {id: 3, name: 'John'},
-    {id: 4, name: 'Matt'},
-];
 
-var b = [
-    {id: 1, name1: 'Mathew', position: '1'},
-    {id: 2, name1: 'Gracia', position: '2'},
-    {id: 3, name1: 'John', position: '2'},
-    {id: 4, name1: 'Matt', position: '2'},
-];
+gebruikers = [{id:1, name:"hop" },{id:2, name:"michael" },{id:3, name:"shannon" }];
+rows = [{id:1, aantal:5 },{id:2, aantal:3 },{id:1, aantal:5 },{id:3, aantal:5 },{id:1, aantal:5 }];
 
 
-function innerjoin(array1,array2,id,id){
-    var result = [];
-    array1.forEach(function (v) {
-        array2.forEach(function (w) {
-            if (v === w);
-            result.push(v)
-            console.log(result);
-        })
 
-    })
+function join(users,data){
+for (i = 0; i < data.length; i++) {
+    user_id = data[i].id;
+    data[i].id = users[user_id - 1].name;
+}
+return data
 }
 
-//innerjoin(a,b);
 
 
 
-var i;
-for (i = 0; i < b.length; i++) {
-    result[i] = 
-}
+console.log(join(gebruikers,rows));
